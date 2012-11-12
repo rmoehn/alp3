746f3bee2467630a1af6500745543434631bd974
Richard Möhn <richard.moehn@fu-berlin.de>
2012-11-12 18:06:07 +0100

    Add still incorrect tests for Quickselect

---
+++ 03-01-quickselect/src/test/java/alp3/ueb/QuickselectTest.java
@@ -0,0 +1,72 @@
+package alp3.ueb;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import java.util.Random;
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
+
+public class QuickselectTest {
+    public int[] sizes = { 1, 2, 3, 5, 8, 13, 21, 34 };
+    public List<List<Integer>> testlists;
+    public List<List<Integer>> sortedTestlists;
+
+    @Before public void setUp() {
+        Random random   = new Random();
+        testlists       = new ArrayList(sizes.length);
+        sortedTestlists = new ArrayList(sizes.length);
+
+        // Create sorted and an unsorted versions of random lists
+        for (int size : sizes) {
+            List<Integer> testlist = randomList(size);
+                // Gibt's da was von Commons?
+            testlists.add(testlist);
+
+            List<Integer> sortedTestlist = new ArrayList(testlist);
+            Collections.sort(sortedTestlist);
+            sortedTestlists.add(sortedTestlists);
+        }
+    }
+
+    @Test public void testAllSizes() {
+        // Go through lists of various sizes
+        for (int i = 0; i < sizes.length; ++i) {
+            // Get the lists for the current length
+            List<Integer> testlist       = testlists.get(i);
+            List<Integer> sortedTestlist = sortedTestlists.get(i);
+
+            // Test the correct the selection of every k-th smallest element
+            for (int k = 0; k < testlist.size(); ++k) {
+                assertEquals(
+                    sortedTestlist.get(k),
+                        // Funktioniert so noch nicht. Entweder mit
+                        // irgendeinem reduce oder irgendeinem anderen select.
+                    Quickselect.select(k, testlist)
+                );
+            }
+        }
+    }
+
+
+    // Test with empty list
+    @Test(expected=IllegalArgumentException.class)
+    public void testException0() {
+        Quickselect.select(1, new ArrayList<Integer>());
+    }
+
+    // Test with too small index
+    @Test(expected=IllegalArgumentException.class)
+    public void testException1() {
+        Quickselect.select(-10, testlists.get(2));
+    }
+
+    // Test with too large index
+    @Test(expected=IllegalArgumentException.class)
+    public void testException2() {
+        List<Integer> testlist = testlists.get(3);
+        Quickselect.select(testlist.size(), testlist);
+    }
+
+}
