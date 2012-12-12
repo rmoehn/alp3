ee8347b592389fb84f52cac6cd6a8e3f2a666420
Richard Möhn <richard.moehn@fu-berlin.de>
2012-12-12 10:31:52 +0100

    Stringsuche und Tests fertig

--- 07-02-stringsearch/pseudo-grep.c
+++ 07-02-stringsearch/pseudo-grep.c
@@ -8,7 +8,7 @@ void pseudo_grep(const char *, int, FILE *);
 int main(int argc, const char *argv[])
 {
     // Check command line arguments
-    if (argc == 1) {
+    if (argc == 2) {
         errx(ARG_ERROR, "There must be at least two command line arguments");
     }
 
@@ -16,13 +16,7 @@ int main(int argc, const char *argv[])
     int pat_length       = strlen(pattern);
         // Not quite from stdio.h, but not part of the task
 
-    // If we have just the pattern as argument
-    if (argc == 2) {
-        // Search stdin
-        pseudo_grep(pattern, pat_length, stdin);
-    }
-
-    // Otherwise open the given file (further arguments are ignored)
+    // Open the given file (further arguments are ignored)
     FILE *stream = fopen(argv[2], "r");
     if (stream == NULL) {
         err(ARG_ERROR, "Cannot open %s for reading", argv[2]);
@@ -57,19 +51,14 @@ void pseudo_grep(const char pattern[], int length, FILE *stream)
      *
      * The behaviour for patterns containing \n is undefined.
      */
-    long from_linestart = 0;
-    int matched_chars   = 0;
+    long offset       = 0;
+    int matched_chars = 0;
 
     // Read the stream character by character
     int cur_char;
     while ((cur_char = fgetc(stream)) != EOF) {
         // Increment number of characters read since last \n
-        ++from_linestart;
-
-        // Reset that number if we're at the end of a line
-        if (cur_char == '\n') {
-            from_linestart = 0;
-        }
+        ++offset;
 
         // If the current character matches the next character from pattern
         if (cur_char == pattern[matched_chars]) {
@@ -78,47 +67,28 @@ void pseudo_grep(const char pattern[], int length, FILE *stream)
 
             // If we have the whole pattern matched
             if (matched_chars == length) {
-                // Go back to the beginning of the line
-                if (fseek(stream, -from_linestart, SEEK_CUR) == -1) {
-                    printf("%d", (int) from_linestart);
-                    err(INPUT_ERROR, "Cannot go back to last newline");
-                }
-
-                // Read characters
-                int line_char;
-                while ((line_char = fgetc(stream)) != EOF) {
-                    // Print the to standard output
-                    if (fputc(line_char, stdout) == EOF) {
-                        err(OUTPUT_ERROR, "Error writing to stdout");
-                    }
-
-                    // Abort printing on the end of the line
-                    if (line_char == '\n') {
-                        break;
-                    }
-                }
-                if (ferror(stream)) {
-                    err(INPUT_ERROR, "Error in reading");
-                }
-
-                // Reset the pattern pointer and the position in the line
-                matched_chars  = 0;
-                from_linestart = 0;
+                // Print the offset of the match
+                printf("%ld ", offset - (matched_chars - 1));
             }
         }
-        // If we have a mismatch after some characters of pattern matched
-        else if (cur_char != pattern[matched_chars] && matched_chars > 0) {
-            // Rewind to the position after the first matching character
+        // If we had a full match or a mismatch after a partial match
+        else if (cur_char == pattern[matched_chars]
+                 || (cur_char != pattern[matched_chars]
+                     && matched_chars > 0)) {
+            // Rewind to the position of the first matching character
             if (fseek(stream, -matched_chars, SEEK_CUR) == -1) {
                 err(INPUT_ERROR, "Cannot go back in input stream");
             }
 
             // Reset number of characters matched and line offset
-            from_linestart -= matched_chars;
-            matched_chars   = 0;
+            offset        -= matched_chars;
+            matched_chars = 0;
         }
     }
     if (ferror(stream)) {
         err(INPUT_ERROR, "Error in reading");
     }
+
+    // Append newline to the list of offsets
+    printf("\n");
 }
